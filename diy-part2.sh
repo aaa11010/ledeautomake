f3f02d6501d01d7613192b3d2b7009ddfdd4c028
aaa11010 <63449902+aaa11010@users.noreply.github.com>
2025-06-24 06:00:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,9 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.25.41/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+sed -i 's/OpenWrt/HUAWEI Pura X/g' package/base-files/files/bin/config_generate
